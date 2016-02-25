e7e847ac4320d610938d4787503a3144c666bf45
Lukas Berwanger <info@derbl4ck.com>
2016-02-25 20:05:40 +0100

    style things

--- bubble_sort.java
+++ bubble_sort.java
@@ -5,22 +5,22 @@ public class bubble_sort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		 int Array[] = new int[]{6,40,70,55,2,9,54,34,22,78,65,245};
+		int Array[] = new int[]{6,40,70,55,2,9,54,34,22,78,65,245};
 		 
-		 System.out.println("This is the Array Before Bubble Sort");
-		 
-         for(int i=0; i < Array.length; i++){
-        	 System.out.print(Array[i] + " ");
-         }
+		System.out.println("This is the Array Before Bubble Sort");
+ 
+		for(int i=0; i < Array.length; i++) {
+			System.out.print(Array[i] + " ");
+		}
         
-         // sort the array using bubble sort algorithm
-         bubbleSort(Array);
-        
-         System.out.println("");
-         System.out.println("Array After Bubble Sort");
-         for(int i=0; i < Array.length; i++){
-        	 System.out.print(Array[i] + " ");
-         }
+		// sort the array using bubble sort algorithm
+		bubbleSort(Array);
+	
+		System.out.println("");
+		System.out.println("Array After Bubble Sort");
+		for(int i=0; i < Array.length; i++) {
+			System.out.print(Array[i] + " ");
+		}
 	}
 	
 	
