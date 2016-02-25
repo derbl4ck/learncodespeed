6249f7b9eb8d3fc9fdc9ce339b9fab6d583440e2
Lukas Berwanger <info@derbl4ck.com>
2016-02-25 20:01:54 +0100

    style things

--- insertion_sort.java
+++ insertion_sort.java
@@ -5,23 +5,23 @@ public class insertion_sort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		 int Array[] = new int[]{6,40,70,55,2,9,54,34,22,78,65,245};
+		int Array[] = new int[]{6,40,70,55,2,9,54,34,22,78,65,245};
 		 
-		 System.out.println("This is the Array Before Bubble Sort");
+		System.out.println("This is the Array Before Bubble Sort");
 		 
-         for(int i=0; i < Array.length; i++){
-        	 System.out.print(Array[i] + " ");
-         }
+		for(int i=0; i < Array.length; i++) {
+			System.out.print(Array[i] + " ");
+        	}
 		
 		// sort the array using insertion sort algorithm
 		insertionSort(Array);
 
 		System.out.println("");
-        System.out.println("Array After Bubble Sort");
-        for(int i=0; i < Array.length; i++){
-        	System.out.print(Array[i] + " ");
-        }
-
+		System.out.println("Array After Bubble Sort");
+		
+		for(int i=0; i < Array.length; i++) {
+			System.out.print(Array[i] + " ");
+		}
 	}
 
 	/**
